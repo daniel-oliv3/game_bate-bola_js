d1fc7f7995ca8bd9ccc696b92b06f380eb50484f
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-06 19:06:54 -0300

    Estrutura básica

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 (function(){
 
+    var cnv = document.querySelector('canvas');
+    var ctx = cnv.getContext('2d');
+
+
+    //Funções
+    function loop(){
+        requestAnimationFrame(loop, cnv);
+        update();
+        render();
+    }
+
+    function update(){
+        
+    }
+
+    function render(){
+        
+    }
+
+    loop();
+
 }());
